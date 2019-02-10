e41a24eba7445a28484af689daae0e18624a56bc
BigAgnes <K.Tc.7@mail.ru>
2019-02-10 18:33:25 +0300

    домашнее задание к уроку №7

--- Lesson_7/hw07_normal.py
+++ Lesson_7/hw07_normal.py
@@ -6,3 +6,23 @@
 Метод __add__ должен реализовывать сложение матриц, а __str__ - вывод итоговой
 матрицы.
 '''
+
+class Matrix(list):
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.x = matrix[0]
+        self.y = matrix[1]
+        self.z = matrix[2]
+    def print(self):
+        for el in self.matrix:
+            print (el)
+    def __add__(self, other):
+        return ((self.x + other.x), (self.y + other.y), (self.z + other.z))
+
+g1 = Matrix([[1, 2, 3], [5, 4, 6], [8, 9, 7]])
+g2 = Matrix([[3, 2, 1], [6, 4, 5], [7, 9, 8]])
+print(g1.print())
+g3 = g1 + g2
+print(g3)
+
+
